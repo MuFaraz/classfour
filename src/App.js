9d2833eae6dd7770dd8e3ef65eae7877263cded9
Muhammad Faraz <muhammadfaraz991@gmail.com>
2020-07-18 17:08:45 +0500

    updated Assignment 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import {Message} from './message.js';
 import './App.css';
 
 function App() {
+  let [count,setCount] = useState(2);
+  let [isMorning,setMorning] = useState(true);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isMorning ? 'dayLight':''}`}>
+<h1>Day time is {isMorning ? 'MORNING':'NIGHT'}</h1>
+<Message counter={count}/>
+      <h1>The value of Count is app.js file{count}</h1>
+      <button onClick={()=> setCount(count + 1)}>Counter</button>
+      <button onClick={()=> setMorning(!isMorning)}>Change Day</button>
+      
     </div>
   );
 }
---
+++ src/message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message(props){
+    return (
+<h1>The value of counter is message.js file {props.counter}</h1>
+    );
+}
